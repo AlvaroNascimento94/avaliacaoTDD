51807fb42383dbb1255d0aa47479dfa3653de7ae
AlvaroNascimento94 <alvarosum@hotmail.com>
2025-11-07 19:29:33 -0300

    RED: Cria teste para depósito válido

--- ContaBancaria.test.js
+++ ContaBancaria.test.js
@@ -4,3 +4,9 @@ test('deve criar uma conta com saldo inicial zero', () => {
     const conta = new ContaBancaria();
     expect(conta.saldo).toBe(0);
 });
+
+test('deve realizar um depósito válido', () => {
+    const conta = new ContaBancaria();
+    conta.depositar(100);
+    expect(conta.saldo).toBe(100);
+});
